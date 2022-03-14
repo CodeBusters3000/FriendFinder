f663dd7903b7c2daef4b0e3958df346935f1934a
Kylian Lichtensteiger <Kylian.Lichtensteiger@bsi-software.com>
2022-03-14 11:23:49 +0100

    Init of Spring Boot project

---
+++ src/main/java/com/codebusters3000/friendfinder/FriendfinderApplication.java
@@ -0,0 +1,13 @@
+package com.codebusters3000.friendfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebusters3000/friendfinder/FriendfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebusters3000.friendfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
